cf68d5b9f4ef44cd5fa0c458b4eff063b0645426
Felipe Nascimento <felipe.nasci.dev@gmail.com>
2025-06-19 00:12:00 -0300

    reverse deploy settings

--- setup/settings.py
+++ setup/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#szxu4-n07+5-)w5x$x!)--c6q(izu-=v*wcgd41^is5j8e-7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -109,7 +109,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
