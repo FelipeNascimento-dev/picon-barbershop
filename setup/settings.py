bfbe85d0d723880c5f01f6382dc084eb28dcbf27
Felipe Nascimento <felipe.nasci.dev@gmail.com>
2025-06-18 23:27:12 -0300

    teste de deploy

--- setup/settings.py
+++ setup/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#szxu4-n07+5-)w5x$x!)--c6q(izu-=v*wcgd41^is5j8e-7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
